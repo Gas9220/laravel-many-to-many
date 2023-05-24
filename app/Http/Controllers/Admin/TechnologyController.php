354da8d3c07d5cfc406d230227e0b2871aacc920
Gaspare <Gas9220@users.noreply.github.com>
2023-05-24 09:57:35 +0200

    technology delete operation

--- app/Http/Controllers/Admin/TechnologyController.php
+++ app/Http/Controllers/Admin/TechnologyController.php
@@ -80,5 +80,8 @@ class TechnologyController extends Controller
      */
     public function destroy(Technology $technology)
     {
+        $technology->delete();
+
+        return redirect()->route('admin.technologies.index')->with('message', "Tecnologia cancellata con successo");
     }
 }
